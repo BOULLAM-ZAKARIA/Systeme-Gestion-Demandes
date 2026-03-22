37911eef10836e415b7ae64ba1b5895f41cd240e
Zakaria Boullam <Zakaria.BOULLAM@emines.um6p.ma>
2026-03-22 18:44:19 +0100

    fix: use connection pool for MySQL

--- router.js
+++ router.js
@@ -40,20 +40,23 @@ router.get('/table', (_req, res) => {
     res.render('table');
 });
 
-//Définir la connection avec le server Mysql
-const connection = mysql.createConnection({
+//Définir le pool de connections MySQL
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-//Etablir la connection
-connection.connect((err) => {
+connection.getConnection((err, conn) => {
     if (err) {
         console.error("MySQL connection failed:", err.message);
     } else {
         console.log("connected successfully!");
+        conn.release();
     }
 });
 
